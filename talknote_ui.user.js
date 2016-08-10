6331f2ff0bb7a157e9f22acabfbabde45314bc4c
八代　将澄 <yatsushiro@mediba.jp>
2016-08-10 18:54:46 +0900

    ajax対応した

--- talknote_ui.user.js
+++ talknote_ui.user.js
@@ -7,44 +7,115 @@
 // @author       Yatsushiro
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
-// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
+// @grant         GM_addStyle
+// @grant         GM_getValue
+// @grant         GM_setValue
 // ==/UserScript==
-$('.container_table').ready(function(){
-        $(this).css({
-            'padding-top': '10px'
-        });
-    });
-$(function() {
+(function($) {
+  // $(document).ajaxComplete(function(){
+  //
+  //   // タイムライン削除
+  //   $('#left_menu_allfeed').css({
+  //       'display': 'none',
+  //   });
+  //
+  //   // コメント部分の半透明化
+  //   $('.comment_status, .good_status').css({
+  //       'background-color': 'rgba(255,255,255,0.2)'
+  //   });
+  //
+  //   // 右サイドメニューのコンテンツ削除( ご意見ご要望欄 )
+  //   $('.right-nav-content-header, .right_nav_cont').css({'display': 'none'});
+  //
+  //   // right_nav_tasks
+  //   $('.comment_status_now, #task_list_box').css({
+  //       'background': 'rgba(255,255,255,0.4)'
+  //   });
+  //
+  //   $('dl').removeClass('comment_status_readmore_container');
+  //
+  // });
+
+
+  // GM_setValue("imageString", null);
+
+  // if(!GM_getValue('imageString')) {
+  //
+  //   $("head").append('<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.min.css" >');
+  //   /*jshint multistr: true */
+  //   $("body").append('\
+  //   <div id=dialog style=display:none>\
+  //   <form>\
+  //     <fieldset>\
+  //       <label for="text">背景画像URL</label>\
+  //         <input type="text" name="url" id="url">\
+  //     </fieldset>\
+  //   </form>\
+  //   </div>\
+  //   ');
+  //
+  //   // dialog表示
+  //   $('#dialog').dialog({
+  //     title: "背景画像選択",
+  //     buttons: {
+  //       "OK": function() {
+  //         $(this).dialog( "close" );
+  //         GM_setValue('imageString', $('#url').val());
+  //         alert('背景画像を適応しました。リロードしてください。');
+  //       },
+  //     },
+  //   });
+  //
+  // }
+
+  imageUrl = GM_getValue('imageString')
+
+  if(imageUrl) {
     // 背景変更
     $('body').css({
-        'background-image': 'url(http://freebies-db.com/wp-content/uploads/2014/02/free-textures-seamless-polygon-backgrounds-graphicburger.jpg)',
+        'background-image': 'url('+ imageUrl +')',
         'background-size': 'cover',
         'background-position': 'left top'
     });
+  }
 
-    // 左サイドメニューの位置修正と背景透過
-    $('#left_nav').css({
-        'background-color': 'rgba(255,255,255,0.5)',
-        'padding-top': '15px'
-    });
-    $('div').removeClass('dm_group_list');
+  // 左サイドメニューの位置修正と背景透過
+  $('#left_nav').css({
+      'background-color': 'rgba(255,255,255,0.5)',
+      'padding-top': '15px'
+  });
+  $('div').removeClass('dm_group_list');
 
-    // メインコンテンツの背景透過
-    $('#wrapper #container').css({
-        'background-color': 'rgba(255,255,255,0.6)',
-    });
+  // メインコンテンツの背景透過
+  $('#wrapper #container').css({
+      'background-color': 'rgba(255,255,255,0.6)',
+  });
 
-    $('.comment_status, .good_status').css({
-        'background-color': 'rgba(255,255,255,0.2)'
-    }); // TODO: 画面遷移した際に変更されない
+})(jQuery.noConflict(true));
 
-    // 右サイドメニューのコンテンツ削除( ご意見ご要望欄 )
-    $('.right-nav-content-header, .right_nav_cont').css({'display': 'none'}); // TODO: 画面遷移した際に変更されない
 
-    // right_nav_tasks
-    $('.comment_status_now, #task_list_box').css({
-        'background': 'rgba(255,255,255,0.4)'
-    }); // TODO: 画面遷移した際に変更されない
 
-    $('dl').removeClass('comment_status_readmore_container'); // TODO: 画面遷移した際に変更されない
-})();
+$(document).ajaxComplete(function(){
+
+  // タイムライン削除
+  $('#left_menu_allfeed').css({
+      'display': 'none',
+  });
+
+  // コメント部分の半透明化
+  $('.comment_status, .good_status').css({
+      'background-color': 'rgba(255,255,255,0.2)'
+  });
+
+  // 右サイドメニューのコンテンツ削除( ご意見ご要望欄 )
+  $('.right-nav-content-header, .right_nav_cont').css({'display': 'none'});
+
+  // right_nav_tasks
+  $('.comment_status_now, #task_list_box').css({
+      'background': 'rgba(255,255,255,0.4)'
+  });
+
+  $('dl').removeClass('comment_status_readmore_container');
+
+});
