761849a7d6c8db02ed95865cf1771e4f8c3570cc
八代　将澄 <yatsushiro@mediba.jp>
2016-08-10 11:20:13 +0900

    sharepoint script add

---
+++ sharepointscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         share point script
+// @version      0.1
+// @description  利便性向上させちゃう
+// @match      https://medibasys.sharepoint.com/org/syslog/Lists/Posts/EditPost.aspx?*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @author      Yatsushiro
+// @grant        none
+// ==/UserScript==
+
+(function($) {
+  $('.ms-formtable .ms-formbody').css({
+    'width': '100%'
+  });
+  $('.ms-rtestate-field ms-rtefield ms-inputBox').css({
+    'width': '100%'
+  });
+})(jQuery.noConflict(true));
