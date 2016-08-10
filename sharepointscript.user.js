fc062ec45c0eb17d934cb509f25057bf7e4e9a8d
八代　将澄 <yatsushiro@mediba.jp>
2016-08-10 11:33:22 +0900

    インデントが気になった

--- sharepointscript.user.js
+++ sharepointscript.user.js
@@ -2,9 +2,9 @@
 // @name         share point script
 // @version      0.1
 // @description  利便性向上させちゃう
-// @match      https://medibasys.sharepoint.com/org/syslog/Lists/Posts/EditPost.aspx?*
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @author      Yatsushiro
+// @match        https://medibasys.sharepoint.com/org/syslog/Lists/Posts/EditPost.aspx?*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @author       Yatsushiro
 // @grant        none
 // ==/UserScript==
 
