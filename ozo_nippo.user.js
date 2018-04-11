d8f34de6fe0cddeecac53fc9ccdcecd61f27e32f
八代 将澄 <yatsushiro@mediba.jp>
2018-04-11 13:55:44 +0900

    wip ozo_nippo ばーじょんあっぷ

--- ozo_nippo.user.js
+++ ozo_nippo.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         おぞにっぽー(ﾟ∀ﾟ)
 // @namespace    https://github.com/YatsushiroMasato/TamperScript
-// @version      ばーじょんわん！
-// @description  OZOから勤怠情報をしてきて日報に反映させるかんじのやつ
+// @version      ばーじょんつー！
+// @description  OZOの勤怠情報をよしなに日報反映させるかんじのやつ
 // @author       Masato Yatsushiro <yatsushiro@mediba.jp>
 // @match        https://manage.ozo-cloud.jp/mediba/*
-// @match        http://mediba-pjtweb01/ip_web/scripts/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
@@ -13,67 +12,38 @@
 (function() {
     'use strict';
 
-    if (window.location.href.indexOf('https://manage.ozo-cloud.jp/mediba/') == 0) {
-
-        if (document.getElementsByClassName('BaseFontFam')[1].textContent == "簡易表示") {
-            getKintai();
-        }
-
-        function getKintai() {
-            var target_id = 'db_NIPPOU_SINSEI_DATE_';
-            var target_date = 0;
-
-            var work_time = [];
-            var work_in, work_out, work_date;
-            var table = document.getElementById(target_id+1).parentNode.parentNode.parentNode.parentNode.rows;
-            for (var i = 0; i < table.length; i++) {
-                if (table[i].firstElementChild.firstElementChild) {
-                    target_date++;
-                    var element = document.getElementById(target_id+target_date);
-
-                    work_date = element.value;
-                    work_in = element.parentNode.parentNode.parentNode.nextElementSibling.children[2].textContent.trim();
-                    work_out = element.parentNode.parentNode.parentNode.nextElementSibling.children[3].textContent.trim();
-
-                    if (work_in.match(/-/)){
-                        work_in = work_in.replace( /-/g , '' ) ;
-                    }
-                    if (work_out.match(/-/)){
-                        work_out = work_out.replace( /-/g , '' ) ;
-                    }
-                    
-                    work_time.push(work_date+"@"+work_in+","+work_out);
-                }
-            }
-
-            GM_setValue('work_time', work_time);
-            console.log(GM_getValue('work_time'));
-        }
-
-    } else if (window.location.href.indexOf('http://mediba-pjtweb01/ip_web/scripts/') == 0) {
-        var work_time = GM_getValue('work_time');
-        var keisan = document.getElementById('btnCalcJitsudouJikan');
-
-        if (work_time && keisan) {
-            var chkdate = document.getElementById('txtKaishiJikan').parentNode.parentNode.previousElementSibling.previousElementSibling.children[1].textContent.trim();
-            for (var i in work_time) {
-                var work_date = work_time[i].split('@')[0];
-
-                if (chkdate == work_date) {
-                    var kaishi = document.getElementById('txtKaishiJikan');
-                    var shuryo = document.getElementById('txtShuryoJikan');
-                    var kyukei = document.getElementById('txtKyukeiJikan');
-
-                    kaishi.value = work_time[i].split('@')[1].split(',')[0];
-                    shuryo.value = work_time[i].split('@')[1].split(',')[1];
-                    kyukei.value = "1:00";
-
-                    if (document.getElementById('btnCalcJitsudouJikan').parentNode.textContent.slice(-5).trim() == '時間 ：') {
-                        keisan.click();
-                    }
-                    break;
-                }
-            }
-        }
+    // getパラメータを取得
+    var param_arr = {};
+    var get_params = location.search.substring(1).split('&');
+    for(var i = 0; i < get_params.length; i++) {
+        var keySearch = get_params[i].search(/=/);
+        var key = '';
+        if(keySearch != -1) key = get_params[i].slice(0, keySearch);
+        var val = get_params[i].slice(get_params[i].indexOf('=', 0) + 1);
+        if(key != '') param_arr[key] = decodeURI(val);
     }
+
+    // 日報入力時の画面に遷移した際のみ処理する
+    if(param_arr.fuseaction === 'clz_TemplateProcessRequest') {
+        // 画面ロードされるまで処理まちさせる
+        var wait_page = setInterval(function(){
+            if (document.getElementById('layer').className == "layer complete") {
+                console.log("page loaded!");
+                // ループ処理clear
+                clearInterval(wait_page);
+                // 実処理
+                nippo();
+            }
+        },1000);
+    }
+
+    // 日報計算処理などなど
+    function nippo() {
+        // iframeのコンテンツを取得
+        var contents = document.getElementById('exiframe').contentDocument;
+
+        // debug
+        console.log(contents.getElementById('fidProjectCode_1'));
+    }
+
 })();
